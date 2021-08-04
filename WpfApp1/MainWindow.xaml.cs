f565392165c2dc9e5ed3e97e5f26f342f034c9d1
Kamran999A <85412897+Kamran999A@users.noreply.github.com>
2021-08-04 20:48:13 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using Microsoft.Toolkit.Wpf.UI.Controls;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        private readonly Random _random = new Random();
+        private const int StackPanelSecondFirstButtonUID = 4;
+        private const int ClearAllButtonUID = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void MapControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            BasicGeoposition chennaiGeoposition = new BasicGeoposition() { Latitude = 13.0827, Longitude = 80.2707 };
+            var center = new Geopoint(chennaiGeoposition);
+
+            await ((MapControl)sender).TrySetViewAsync(center, 12);
+        }
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                Color randomColor = Color.FromRgb((byte)_random.Next(0, 255), (byte)_random.Next(0, 255), (byte)_random.Next(0, 255));
+                button.Background = new SolidColorBrush(randomColor);
+                MessageBox.Show($"My number is {button.Content}", "Information about button", MessageBoxButton.OK, MessageBoxImage.Information);
+                Window1 win1 = new Window1();
+                win1.Show();
+            }
+        }
+        private void RightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                Title = $"Button Content  \"{button.Content}\" is Deleted.";
+                int buttonUID = Convert.ToInt32(button.Uid);
+                if (buttonUID == ClearAllButtonUID)
+                {
+                    MainGrid.Children.Clear();
+                }
+                else if (buttonUID < StackPanelSecondFirstButtonUID)
+                {
+                    StackPanelFirst.Children.Remove(button);
+                }
+                else
+                {
+                    StackPanelSecond.Children.Remove(button);
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using Microsoft.Toolkit.Wpf.UI.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            var mControl = new MapControl();
+            mControl.MapServiceToken = "YyCLZ0tUWEVR5LhmOxU2~N5_DKq3BO09wuiDyyYk7cA~AnIe2DUX0rrV-lMgFs_kXLA5gEViyUs6v0AiM0La8hkXgQ2ydTUYo5mi8VO7koIu";
+            mControl.Loaded += mapControl_Loaded;
+            mainGrid.Children.Add(mControl);
+        }
+        private async void mapControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 40.41492, Longitude = 49.85320 };
+            var cityCenter = new Geopoint(cityPosition);
+            await (sender as MapControl).TrySetViewAsync(cityCenter, 19);
+        }
+    }
+}
